157254bff142fbdbabace929ba0f61cd3cb5b269
Ezmiron Deniku <ezmiron.deniku@studio.unibo.it>
2023-08-13 16:42:52 +0200

    modify user dropdown profile

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -78,21 +78,30 @@
                                     <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
                                 </li>
                             @endif
-                        @else
+                        @else  
                             <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->name }}
+                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button"
+                                    data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>{{ Auth::user()->name }}
                                 </a>
-
-                                <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                        {{ __('Logout') }}
+                            
+                                <div class="dropdown-menu dropdown-menu-right fade-in" aria-labelledby="navbarDropdown">
+                                    <a class="dropdown-item" href="{{-- route('profile', auth()->user()->username) --}}">
+                                        Profile
                                     </a>
-
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                            
+                                    <a class="dropdown-item" href="{{-- route('settings') --}}">
+                                        Settings
+                                    </a>
+                            
+                                    <hr class="dropdown-divider">
+                            
+                                    <form method="POST" action="{{ route('logout') }}">
                                         @csrf
+                            
+                                        <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();
+                                                this.closest('form').submit();">
+                                            Logout
+                                        </a>
                                     </form>
                                 </div>
                             </li>
